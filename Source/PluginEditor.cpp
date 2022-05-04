12cd58b54402ba701174d83f3e0a384c9057c066
alecgreene <acgreene@umich.edu>
2022-05-04 13:58:25 -0400

    added sliders to gui

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,12 @@
 RuckusEQAudioProcessorEditor::RuckusEQAudioProcessorEditor (RuckusEQAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    //batch add all of the sliders to the gui
+    for(auto* comp: getComps())
+    {
+        addAndMakeVisible(comp);
+    }
+    setSize (800, 533);
 }
 
 RuckusEQAudioProcessorEditor::~RuckusEQAudioProcessorEditor()
@@ -33,8 +36,67 @@ void RuckusEQAudioProcessorEditor::paint (juce::Graphics& g)
     g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
+//here is where the positions of the gui components are layed out in the plugin window
 void RuckusEQAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    //boundary of the entire plugin window
+    auto bounds = getLocalBounds();
+    
+    //allocate top 40% of the plugin window for the frequency response curve
+    auto responseArea = bounds.removeFromTop(bounds.getHeight() * 0.6);
+    
+    //allocate left 12.5% of bottom of window for HPF
+    auto highPassArea = bounds.removeFromLeft(bounds.getWidth() * 0.125);
+    highPassFreqSlider.setBounds(highPassArea.removeFromTop(highPassArea.getHeight() * 0.7));
+    highPassSlopeSlider.setBounds(highPassArea);
+    
+    auto rumbleArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/87.5));
+    rumbleFreqSlider.setBounds(rumbleArea.removeFromTop(rumbleArea.getHeight() * 0.4));
+    rumbleGainSlider.setBounds(rumbleArea.removeFromTop(rumbleArea.getHeight() * 0.667));
+    rumbleQualitySlider.setBounds(rumbleArea);
+    
+    auto lowArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/75));
+    lowFreqSlider.setBounds(lowArea.removeFromTop(lowArea.getHeight() * 0.4));
+    lowGainSlider.setBounds(lowArea.removeFromTop(lowArea.getHeight() * 0.667));
+    lowQualitySlider.setBounds(lowArea);
+
+    auto lowMidArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/62.5));
+    lowMidFreqSlider.setBounds(lowMidArea.removeFromTop(lowMidArea.getHeight() * 0.4));
+    lowMidGainSlider.setBounds(lowMidArea.removeFromTop(lowMidArea.getHeight() * 0.667));
+    lowMidQualitySlider.setBounds(lowMidArea);
+
+    auto highMidArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/50));
+    highMidFreqSlider.setBounds(highMidArea.removeFromTop(highMidArea.getHeight() * 0.4));
+    highMidGainSlider.setBounds(highMidArea.removeFromTop(highMidArea.getHeight() * 0.667));
+    highMidQualitySlider.setBounds(highMidArea);
+
+    auto highArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/37.5));
+    highFreqSlider.setBounds(highArea.removeFromTop(highArea.getHeight() * 0.4));
+    highGainSlider.setBounds(highArea.removeFromTop(highArea.getHeight() * 0.667));
+    highQualitySlider.setBounds(highArea);
+
+    auto airArea = bounds.removeFromLeft(bounds.getWidth() * (12.5/25));
+    airFreqSlider.setBounds(airArea.removeFromTop(airArea.getHeight() * 0.4));
+    airGainSlider.setBounds(airArea.removeFromTop(airArea.getHeight() * 0.667));
+    airQualitySlider.setBounds(airArea);
+    
+    auto lowPassArea = bounds.removeFromRight(bounds.getWidth() * 1);
+    lowPassFreqSlider.setBounds(lowPassArea.removeFromTop(lowPassArea.getHeight() * 0.7));
+    lowPassSlopeSlider.setBounds(lowPassArea);
+
+}
+
+std::vector<juce::Component*> RuckusEQAudioProcessorEditor::getComps()
+{
+    return
+    {
+        &highPassFreqSlider,
+        &rumbleFreqSlider, &rumbleGainSlider, &rumbleQualitySlider,
+        &lowFreqSlider, &lowGainSlider, &lowQualitySlider,
+        &lowMidFreqSlider, &lowMidGainSlider, &lowMidQualitySlider,
+        &highMidFreqSlider, &highMidGainSlider, &highMidQualitySlider,
+        &highFreqSlider, &highGainSlider, &highQualitySlider,
+        &airFreqSlider, &airGainSlider, &airQualitySlider,
+        &lowPassFreqSlider, &highPassSlopeSlider, &lowPassSlopeSlider
+    };
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,30 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomRotarySlider : juce::Slider
+{
+    CustomRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
+struct CustomHorizontalSlider : juce::Slider
+{
+    CustomHorizontalSlider() : juce::Slider(juce::Slider::SliderStyle::LinearHorizontal, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
+struct CustomHorizontalBar : juce::Slider
+{
+    CustomHorizontalBar() : juce::Slider(juce::Slider::SliderStyle::LinearBar, juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +52,27 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     RuckusEQAudioProcessor& audioProcessor;
+    
+    CustomRotarySlider  highPassFreqSlider,
+                        rumbleFreqSlider, rumbleGainSlider,
+                        lowFreqSlider, lowGainSlider,
+                        lowMidFreqSlider, lowMidGainSlider,
+                        highMidFreqSlider, highMidGainSlider,
+                        highFreqSlider, highGainSlider,
+                        airFreqSlider, airGainSlider,
+                        lowPassFreqSlider;
+    
+    CustomHorizontalBar  rumbleQualitySlider,
+                        lowQualitySlider,
+                        lowMidQualitySlider,
+                        highMidQualitySlider,
+                        highQualitySlider,
+                        airQualitySlider;
+    
+    CustomHorizontalSlider highPassSlopeSlider, lowPassSlopeSlider;
+    
+    //function that will put all the sliders in a vector so we can iterate through them easily and apply processing on them as a batch if needed.
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RuckusEQAudioProcessorEditor)
 };
